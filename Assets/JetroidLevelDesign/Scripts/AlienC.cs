d9a0172fd93fab5ec88b97fc5ab7aeb2fbb21359
CotillionTheRope <dacheson007@gmail.com>
2021-07-19 12:04:17 -0700

    initial commit

---
+++ Assets/JetroidLevelDesign/Scripts/AlienC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienC : MonoBehaviour {
+
+  public float attackDelay = 3f;
+  public GameObject projectile;
+
+  private Animator animator;
+  
+  // Start is called before the first frame update
+  void Start() {
+    animator = GetComponent<Animator>();
+
+    if(attackDelay != 0) {
+      StartCoroutine(OnAttack());
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+    animator.SetInteger("AnimState", 0);
+  }
+
+  IEnumerator OnAttack() {
+    yield return new WaitForSeconds(attackDelay);
+
+    Fire();
+
+    StartCoroutine(OnAttack());
+  }
+
+  void Fire() {
+    animator.SetInteger("AnimState", 1);
+  }
+
+  void OnFire() {
+    if(projectile != null) {
+      var clone = Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+  }
+}
---
+++ Assets/JetroidLevelDesign/Scripts/DoorTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+  public Door door;
+  public bool ignoreTirgger;
+
+  private void OnTriggerEnter2D(Collider2D target) {
+
+    if(ignoreTirgger) {
+      return;
+    }
+
+    if (target.gameObject.tag == "Player") {
+      door.Open();
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D target) {
+
+    if (ignoreTirgger) {
+      return;
+    }
+
+    if (target.gameObject.tag == "Player") {
+      door.Close();
+    }
+  }
+
+  public void Toggle(bool value) {
+    if(value) {
+      door.Open();
+    }
+    else {
+      door.Close();
+    }
+  }
+
+  private void OnDrawGizmos() {
+    Gizmos.color = ignoreTirgger ? Color.gray : Color.green;
+
+    var bc2D = GetComponent<BoxCollider2D>();
+    var pos = bc2D.transform.position;
+
+    var newPos = new Vector2(pos.x + bc2D.offset.x, pos.y + bc2D.offset.y);
+
+    Gizmos.DrawWireCube(newPos, new Vector2(bc2D.size.x, bc2D.size.y));
+  }
+}
